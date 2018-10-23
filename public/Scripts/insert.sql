0dfafe73e18a70120b60aec77edeb2ebcce7e242
Julian David Bayer Gonzalez <jdbayer09@hotmail.com>
2018-10-22 21:47:28 -0500

    SQl

---
+++ public/Scripts/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO `adm_tipo_documento` (`nombreTipoDocumento`,`iniciales`) VALUES ('Cédula de Ciudadanía','C.C.');
+INSERT INTO `adm_tipo_documento` (`nombreTipoDocumento`,`iniciales`) VALUES ('Cédula de Extranjería','C.E.');
+INSERT INTO `adm_tipo_documento` (`nombreTipoDocumento`,`iniciales`) VALUES ('Registro civil de nacimiento','R.C.N');
+INSERT INTO `adm_tipo_documento` (`nombreTipoDocumento`,`iniciales`) VALUES ('Tarjeta de identidad ','T.i');
+INSERT INTO `adm_tipo_documento` (`nombreTipoDocumento`,`iniciales`) VALUES ('NIT ','NIT');
+
+INSERT INTO `adm_accion` (`nombreAccion`,`descAccion`) VALUES ('Crear Usuarios','Permite Crear Usuarios');
+INSERT INTO `adm_accion` (`nombreAccion`,`descAccion`) VALUES ('Modificar Usuarios','Permite Modificar Usuarios');
+INSERT INTO `adm_accion` (`nombreAccion`,`descAccion`) VALUES ('Borrar Usuarios','Permite Borrar Usuarios');
+
+
+
+
