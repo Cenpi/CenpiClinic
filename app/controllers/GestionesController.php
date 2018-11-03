4a2963c1043ce862c7c783e7fac6acfbc43d2593
Harry <harryorrego7@gmail.com>
2018-11-03 17:29:45 -0500

    Vista gestiones y parametrización

---
+++ app/controllers/GestionesController.php
@@ -0,0 +1,15 @@
+<?php
+
+class GestionesController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+        $this->assets
+        ->collection('headercss');
+        
+    }
+
+}
+
